ab976c5242c337cbb2d0ae60f346cac4a0400ab1
Roan Mertens <roanmertens91@gmail.com>
2018-11-27 13:37:45 +0000

    list basics in js

---
+++ list-basics-one.js
@@ -0,0 +1,17 @@
+const todo = ['dishes', 'laundry', 'sleep', 'comedy night']
+// console.log(todo)
+
+// printing in the console
+// console.log('my todo list:', todo)
+
+// change element
+// todo[0] = 'extra dishes'
+
+// add to end of list
+// todo.push('bath')
+
+// deleting from list starting at 1 and removing that one and the next one
+// todo.splice(1,2)
+
+console.log(todo)
+
