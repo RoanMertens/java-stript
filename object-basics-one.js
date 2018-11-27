02346a154b9a8e7aeaaa305052a71299383a97c1
Roan Mertens <roanmertens91@gmail.com>
2018-11-27 15:50:37 +0000

    added function or method in object

---
+++ object-basics-one.js
@@ -0,0 +1,20 @@
+// object
+const car = {
+  motor: 'electric',
+  wheels: 4,
+  color: 'red',
+  brand: 'tesla',
+  motorRunning: false,
+  startCar: function() {
+    // this means the car object
+    this.motorRunning = true
+  }
+}
+
+// turn on car and check if it is running
+// // console.log(car.motorRunning)
+// // car.startCar()
+// console.log(car.motorRunning)
+
+// object data
+// car.brand
