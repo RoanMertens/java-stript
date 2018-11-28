ac36f14bf4451dd4e04a6ae0b9b5f9ad42408105
Roan Mertens <roanmertens91@gmail.com>
2018-11-28 10:43:32 +0000

    made an if else statement example file with one version

---
+++ if-else-basics-one.js
@@ -0,0 +1,8 @@
+testArray = ['item 1', 'item 5', 'item 9']
+
+if (testArray.length > 3) {
+  console.log('Yay! That are a lot of items!')
+} else {
+  console.log('Ugh! There are not enough items!')
+
+}
