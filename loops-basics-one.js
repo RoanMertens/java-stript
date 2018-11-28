072cd545ff7b9430caa552b137ad578f52f50bbf
Roan Mertens <roanmertens91@gmail.com>
2018-11-28 10:20:47 +0000

    made a loop file and added three test cases for the for loop

---
+++ loops-basics-one.js
@@ -0,0 +1,16 @@
+
+// dont forget the semi colon. the comma doesnt work there
+// initializatiion;   condition;    final-expression
+// for (let counter = 0; counter < 3; counter++) {
+//   console.log('Hey!')
+// }
+
+// for (let c = 10; c >= 0; c -= 1) {
+//   console.log('minus!' + c)
+//   // c -= 1 //this works as well
+// }
+
+testArray = ['item 1', 'item 5', 'item 9', 'item 12']
+for (let counter = 0; counter <m testArray.length; counter++) {
+  console.log('Item number ' + (counter + 1) + ' is:', testArray[counter])
+}
